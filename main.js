aa8dc3d743d8b7da85d01ea7a7f57bdd7e7d36b1
Apple Mac Book <applemacbook@Appleui-MacBookPro.local>
2021-10-24 14:39:24 +0900

    .

--- main.js
+++ main.js
@@ -5,30 +5,106 @@ var fs = require('fs');
 
 //필요함수 정의
 function readHTML(name){ //html 읽기
-    html = fs.readFileSync('html/' + name + '.html', 'utf-8');
+    var html = fs.readFileSync('html/' + name + '.html', 'utf-8');
     return html
 }
 function removeTag(text){ //태그 지우기
   return text.replaceAll('<', '&lt').replaceAll('>', '&gt')
 }
+function chatCount(roomName){ //채팅수 확인
+  newChatRoom(roomName)
+  return JSON.parse(fs.readFileSync('data/' + roomName + '/info.json', 'utf-8')).chatCount
+}
+function chatCountAppend(roomName){ //채팅수 증가
+  var infoData = {
+    chatCount: chatCount(roomName) + 1
+  }
+  fs.writeFileSync('data/' + roomName + '/info.json', JSON.stringify(infoData))
+  return infoData.chatCount
+}
+function newChatRoom(roomName){ //새로운 채팅방
+  if(fs.existsSync('data/' + roomName)){
+    return true
+  }
+  else{
+    fs.mkdirSync('data/' + roomName)
+    var infoData = {
+      chatCount: 2
+    }
+    fs.writeFileSync('data/' + roomName + '/info.json', JSON.stringify(infoData))
+    var startMsg = [ 
+      {
+        nickname: removeTag('개발자'),
+        msg: removeTag('여기는 ' + roomName + ' 채팅방입니다.'),
+        id: 0
+      },
+      {
+        nickname: removeTag('개발자'),
+        msg: removeTag('자유롭게 채팅해 주세요 :)'),
+        id: 1
+      }
+    ]
+    fs.writeFileSync('data/' + roomName + '/' + '0.json', JSON.stringify(startMsg))
+  }
+}
+function chatRead(roomName, chat100){
+  if(!fs.existsSync('data/' + roomName + '/' + chat100 + ".json")){
+    return null
+  }
+  var chats = JSON.parse(fs.readFileSync('data/' + roomName + '/' + chat100 + ".json", 'utf-8'))
+  return chats
+}
+function chatSave(roomName, msgData){ //새 채팅 내용 저장
+  newChatRoom(roomName)
+  var count = chatCount(roomName)
+  var fileName = Math.floor(count / 100) //100의 자리 숫자
+  fileName += ".json"
+  if(!fs.existsSync('data/' + roomName + '/' + fileName)){
+    fs.writeFileSync('data/' + roomName + '/' + fileName, '[]')
+  }
+  var chats = JSON.parse(fs.readFileSync('data/' + roomName + '/' + fileName, 'utf-8'))
+  chats[chats.length] = msgData
+  fs.writeFileSync('data/' + roomName + '/' + fileName, JSON.stringify(chats))
+  chatCountAppend(roomName)
+}
  
 //route, routing
 //app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/', function(request, response) {
-    html = readHTML('main')
+    var html = readHTML('main')
     response.send(html)
 });
 
 //소켓 통신
 io.on('connection', (socket) => {
   console.log('connected!')
+  var chatRoomName = '광장'
+
+  //최근 채팅내용 전송
+  now100 = Math.floor(chatCount(chatRoomName) / 100)
+  if(chatCount(chatRoomName) % 100 == 0){
+    now100-=1
+  }
+  socket.emit('chatList', {
+    nowchat100: now100,
+    chatList:chatRead('광장', now100)
+  })
+
   socket.on('chat', (data)=>{
     var msgData = {
       nickname: removeTag(data.nickname),
-      msg: removeTag(data.msg)
+      msg: removeTag(data.msg),
+      id: chatCount(chatRoomName)
     }
-    console.log(msgData.nickname + ": " + msgData.msg)
     io.emit('newchat', msgData)
+    console.log(msgData.nickname + ": " + msgData.msg)
+
+    //데이터 저장하기
+    chatSave(chatRoomName, msgData)
+  })
+
+  socket.on('chatListReq', (data)=>{
+    socket.emit('chatListRes', chatRead(chatRoomName, data))
   })
 })
 
